0d6af2f3e991d9199ea6a76705ec5024aea4be78
FabianG1110 <151580954+FabianG1110@users.noreply.github.com>
2025-09-25 00:00:54 +0000

    Completed Homework 1: Temperature Converter

--- homework_1/src/main.rs
+++ homework_1/src/main.rs
@@ -1,3 +1,41 @@
-fn main() {
-    println!("Hello, world!");
-}
+//Assignment #1
+// const FREEZING_POINT_FAHRENHEIT: f64 = 32.0;
+
+// fn fahrenheit_to_celsius(f: f64) -> f64 {
+//     (f - FREEZING_POINT_FAHRENHEIT) * (5.0 / 9.0)
+// }
+
+// fn celsius_to_fahrenheit(c: f64) -> f64 {
+//     (c * (9.0 / 5.0)) + FREEZING_POINT_FAHRENHEIT
+// }
+
+// fn main() {
+//     let mut temp_f: i32 = 92;
+//     let c = fahrenheit_to_celsius(temp_f as f64);
+//     println!("{}°F = {:.2}°C", temp_f, c);
+
+//     for _ in 0..5{
+//         temp_f += 1;
+//         let c = fahrenheit_to_celsius(temp_f as f64);
+//         println!("{}°F = {:.2}°C", temp_f, c);
+
+//     }
+//      println!("C -> F:");
+    
+//     for _ in 0..6{
+//         let c = fahrenheit_to_celsius(temp_f as f64);
+//         let f = celsius_to_fahrenheit(c);
+//         println!("{:.2}°C -> {:.0}°F", c, f);
+//         temp_f -=1
+//     }
+
+
+// }
+
+
+
+
+
+
+//Assignment #2
+
---
+++ homework_1/target/debug/homework_1.d
@@ -0,0 +1,1 @@
+/workspaces/RUSTfall2025/homework_1/target/debug/homework_1: /workspaces/RUSTfall2025/homework_1/src/main.rs
