d7abb0a46ece1021154ea4990c8a2c35240b418d
FabianG1110 <151580954+FabianG1110@users.noreply.github.com>
2025-09-25 02:47:23 +0000

    Solutions for homework 2 v2

--- homework_2/src/main.rs
+++ homework_2/src/main.rs
@@ -1,36 +1,14 @@
-// //Assignment #1
-// fn concat_strings(s1: &String, s2: &String) -> String{
-//     format!("{}{}", s1, s2)
-// }
+//Assignment #1
+fn concat_strings(s1: &String, s2: &String) -> String{
+    format!("{}{}", s1, s2)
+}
 
-// fn main(){
-//     let s1 = String::from("Hello, ");
-//     let s2 = String::from("World!");
-//     let result = concat_strings(&s1, &s2);
-//     println!("{}", result);
-    
-
-// }
-
-
-
-// //Assignment #2
-// fn clone_and_modify(s: &String) -> String{
-//     let mut cloned = s.clone();
-//     cloned.push_str("World!");
-//     cloned
-// }
-// fn main()
-// {
-//     let s = String::from("Hello, ");
-//     let modified = clone_and_modify(&s);
-//     println!("Original: {}", s);
-//     println!("Modified: {}", modified);
-// }
-
-
-
-
+//Assignment #2
+fn clone_and_modify(s: &String) -> String{
+    let mut cloned = s.clone();
+    cloned.push_str("World!");
+    cloned
+}
 
 //Assignment #3
 fn sum(total: &mut i32, low: i32, high: i32) {
@@ -40,6 +18,19 @@ fn sum(total: &mut i32, low: i32, high: i32) {
     }
 }
 fn main(){
+    println!("Assignment 1");
+    let s1 = String::from("Hello, ");
+    let s2 = String::from("World!");
+    let result = concat_strings(&s1, &s2);
+    println!("{}", result);
+
+    println!("\nAssignment 2");
+    let s = String::from("Hello, ");
+    let modified = clone_and_modify(&s);
+    println!("Original: {}", s);
+    println!("Modified: {}", modified);
+
+    println!("\nAssignment 3");
     let mut total = 0;
     sum(&mut total, 0, 100);
     println!("The sum from 0 to 100 is: {}", total);
