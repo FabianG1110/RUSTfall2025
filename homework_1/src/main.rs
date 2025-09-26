7566a6825d8feac0888df044eaec89b73a7235d3
FabianG1110 <151580954+FabianG1110@users.noreply.github.com>
2025-09-26 02:48:07 +0000

    solutions for homework 1

--- homework_1/src/main.rs
+++ homework_1/src/main.rs
@@ -1,4 +1,4 @@
-//Assignment #1
+// //Assignment #1
 // const FREEZING_POINT_FAHRENHEIT: f64 = 32.0;
 
 // fn fahrenheit_to_celsius(f: f64) -> f64 {
@@ -37,5 +37,83 @@
 
 
 
-//Assignment #2
+// //Assignment #2
+// fn is_even(n: i32) -> bool {
+//     n % 2 == 0
+// }
+
+// fn main(){
+//     let numbers = [3, 5, 10, 15, 22, 33, 40, 55, 60, 77];
+
+//     for &num in numbers.iter() {
+//         if num % 3 == 0 && num % 5 == 0 {
+//             println!("{}: FizzBuzz", num);
+//         } else if num % 3 == 0 {
+//             println!("{}: Fizz", num);
+//         } else if num % 5 == 0 {
+//             println!("{}: Buzz", num);
+//         } else if is_even(num) {
+//             println!("{}: Even", num);
+//         } else {
+//             println!("{}: Odd", num);
+//         }
+//     }
+
+//     let mut sum = 0;
+//     let mut i = 0;
+//     while i < numbers.len() {
+//         sum += numbers[i];
+//         i += 1;
+//     }
+//     println!("Sum of all numbers: {}", sum);
+
+//     let mut largest = numbers[0];
+//     let mut j = 0;
+//     loop {
+//         if numbers[j] > largest {
+//             largest = numbers[j];
+//         }
+//         j += 1;
+//         if j >= numbers.len() {
+//             break;
+//         }
+//     }
+//     println!("Largest number: {}", largest);
+// }
+
+
+//Assignment #3
+fn check_guess(guess: i32, secret: i32) -> i32 {
+    if guess == secret {
+        0
+    } else if guess > secret {
+        1
+    } else {
+        -1
+    }
+}
+
+fn main(){
+    let secret = 22;
+    let mut attempts = 0;
+    let mut guess = 10;
+
+    loop {
+        attempts += 1;
+
+        let result = check_guess(guess, secret);
+
+        if result == 0{
+            println!("Guess {} is correct!", guess);
+            break;
+        }else if result == 1{
+            println!("Guess {} is too high", guess);
+            guess -= 1;
+        }else{
+            println!("Guess {} is too low", guess);
+            guess += 1;
+        } 
+    }
+    println!("It took {} guesses to find the secret!", attempts);
+}
 
